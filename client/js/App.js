12ea05d217f11a645fbdb07bb0ef164b9d21d0f2
lopez567 <spencer.r.lopez@gmail.com>
2016-10-02 19:31:44 -0700

    slight changes

--- client/js/App.js
+++ client/js/App.js
@@ -3,3 +3,20 @@ var socket = io.connect('http://localhost:3000');
 socket.on('connect', function() {
   console.log('connected to server!!');
 });
+
+
+var App = class extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+  
+  render () {
+    return (
+      <div className='app'></div>
+      )
+  }
+
+ };
+
+ ReactDOM.render(<App/>, document.getElementById('app'));
+
