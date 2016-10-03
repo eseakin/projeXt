6fb9af9a87222ebd17401f5b86333bc2d7217cc6
lopez567 <spencer.r.lopez@gmail.com>
2016-10-02 18:33:24 -0700

    Basic set up and server

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('client'));
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('A user connected');
+  
+  socket.on('disconnect', function() {
+    console.log('User disconnected');
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
